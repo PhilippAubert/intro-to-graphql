14a1c7415323806945e0b9f8916d6e4c07e783c3
philipp aubert <p.b.m.aubert@gmail.com>
2021-07-19 19:22:31 +0200

    Add graphql-api-fetch

--- src/Launches.js
+++ src/Launches.js
@@ -8,33 +8,81 @@ import {
   Typography,
 } from "@material-ui/core";
 
+import { useQuery, gql } from "./api";
+import { apis } from "./const";
+import { useState } from "react";
+
 export function Launches() {
+  const [launchesLimit, setLaunchesLimit] = useState(6);
+
+  const { data } = useQuery(
+    gql`
+      query ($launchesLimit: Int) {
+        launchesPast(limit: $launchesLimit) {
+          id
+          mission_name
+          rocket {
+            rocket_name
+          }
+          launch_site {
+            site_name_long
+          }
+          launch_date_local
+          links {
+            flickr_images
+          }
+        }
+      }
+    `,
+    { endpoint: apis.spaceX, variables: { launchesLimit } }
+  );
+
+  console.log(data);
+
   return (
     <>
       <Grid container spacing={3}>
-        <Grid item md={6} lg={4}>
-          <Card>
-            <CardMedia
-              style={{
-                height: 250,
-                backgroundColor: "lightgray",
-              }}
-              image={null}
-              title={null}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                "Mission name"
-              </Typography>
-              <Typography variant="body2">
-                "Rocket" launched "launch date" from "launch site"
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+        {data?.launchesPast.map(
+          ({
+            id,
+            mission_name,
+            launch_date_local,
+            launch_site,
+            rocket,
+            links,
+          }) => (
+            <Grid item md={6} lg={4}>
+              <Card>
+                <CardMedia
+                  style={{
+                    height: 250,
+                    backgroundColor: "lightgray",
+                  }}
+                  image={links.flickr_images?.[0]}
+                  title={null}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {mission_name}
+                  </Typography>
+                  <Typography variant="body2">
+                    {rocket.rocket_name} launched {launch_date_local} from (""){" "}
+                    {launch_site.site_name_long}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          )
+        )}
       </Grid>
       <Box py={5}>
-        <Button size="medium" variant="contained" onClick={() => {}}>
+        <Button
+          size="medium"
+          variant="contained"
+          onClick={() => {
+            setLaunchesLimit(launchesLimit + 6);
+          }}
+        >
           {"Load more"}
         </Button>
       </Box>
